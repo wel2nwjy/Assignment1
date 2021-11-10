3591f4a7485cd6ef2f92227e6e6bf75db7cccd04
shemy <shemysb@gmail.com>
2021-11-10 20:27:38 +0530

    change 2

--- validate.js
+++ validate.js
@@ -1,67 +1,94 @@
-function validateFn(noVal,formtype)
-{ 
-    
-    var valtype="";
-    var vaidation=true;
+function validateFn(noVal,form_type)
+{     
+    var valtype="",flag=0;
+    var vaidation=true,validemail=true,validtel=true,validpassword=true;
     var inpfields=[];
-    // var demostr="";
-    
     inpfields=["Full Name","Email","Mobile No:","Username","Password","Repeat Password"]; 
-
-    // If(formtype === "signupfm")
-    // {
-    //     inpfields=["Full Name","Email","Mobile No:","Username","Password","Repeat Password"]; 
-    // } 
-    // else
-    // {
-    //     inpfields=["Username","Password"]; errormsg6
-    // }
+    const test=form_type;
 
     for(let i=1;i<=noVal;i++)
     {   
-        const val=document.getElementById("inputVal"+i);
-        
+        const val=document.getElementById("inputVal"+i);   
         valtype=val.type;
-        const msg=document.getElementById("errormsg"+i);
 
         if (val.value.trim()==="")
-        {
-            msg.innerHTML = 'Enter '+inpfields[i-1];
-            msg.style.color="red";
-            val.style.border="1px solid red";
+        { 
             vaidation=false;
+            style_invalid(i,val,inpfields[i-1]);
         }
         else
         {
-            msg.innerHTML = "";
-            val.style.border="1px solid green";
+            style_valid(i,val);
 
                         if(valtype=="email")
                             {
-                                // return email_validation();
+                                validemail= email_validation(val.value);
                             }
                         else if(valtype=="tel")
                             {
-                                // return ph_validation();
+                                validtel= ph_validation();
                             }
-                        else
+                        else if(valtype=="password"&&flag==0)
                             {
+                                const n =i+1;
+                                const val1=document.getElementById("inputVal"+n);
 
+                                    if(val.value.trim()!="" && val1.value.trim()!="" && val.value!==val1.value)
+                                    {
+                                        validpassword=false;                                        
+                                        style_password_invalid(i,val1);
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        style_valid(i,val); 
+                                    }
+                                 flag=1;
                             }
         }
-    } 
-
-   return vaidation
-
+    }   
+    if(validemail==false||validtel==false||validpassword==false)
+    {vaidation=false;}
+ return vaidation;
+}
+// ...........................................Email Validation.............................................
+function email_validation(inputValue){
+    
+    return false;
 }
 
-
-function email_validation(){
-    return false
-}
-
+// ...........................................Phone Validation.............................................
 function ph_validation(){
-    return false
+    return false;
 }
+// ...........................................Styling the HTML with Error Message.............................................
+function style_valid(indexval,element)
+{ 
+    const msg=document.getElementById("errormsg"+indexval);
+    msg.innerHTML = "";
+    element.style.border="1px solid green"; 
+}
+
+function style_invalid(indexval,element,fieldname)
+{   
+        const msg=document.getElementById("errormsg"+indexval);
+        msg.innerHTML = 'Enter '+ fieldname;
+        msg.style.color="red";
+        element.style.border="1px solid red"; 
+}
+
+function style_password_invalid(indexval,element)
+{
+    const nextval=indexval+1
+    // console.log(indexval);
+    // console.log(nextval);
+    const msg=document.getElementById("errormsg"+indexval);
+    const msgnxt=document.getElementById("errormsg"+nextval);
+    msgnxt.innerHTML = "";
+    msg.innerHTML = "The Passwords don't match.Try Again "; 
+    msg.style.color="red";
+    element.style.border="1px solid red"; 
+}
+
 
    
